1523d71668ebd481a95e90c381044106db88300b
Krylovsentry <krylovsentry@yandex.ru>
2020-06-02 14:56:56 +0300

    add simple cmd usage

--- jirac.py
+++ jirac.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 
+from argparse import ArgumentParser
 from jira import JIRA
 
 from jirap import JiraProxy
@@ -17,6 +18,7 @@ def create_config(path):
     config.set("Settings", "user_name", "your username")
     config.set("Settings", "password", "your password")
     config.set("Settings", "team", "team assignie codes")
+    config.set("Settings", "component", "сomponent label")
 
     with open(path, "w") as config_file:
         config.write(config_file)
@@ -34,22 +36,31 @@ def crud_config(path):
         config.get("Settings", "user_name"),
         config.get("Settings", "password"),
         config.get("Settings", "team"),
-        config.get("Settings", "brazil_team"),
         config.get("Settings", "component")
     ]
 
 
 if __name__ == "__main__":
     path = "settings.ini"
-    [jira_server, project, user_name, password, team, brazil_team, component] = crud_config(path)
+    [jira_server, project, user_name, password, team, component] = crud_config(path)
 
     # initialize jira api
     jira_options = {'server': jira_server}
     jiraProxy = JiraProxy(
         JIRA(options=jira_options, basic_auth=(user_name, password)),
-        str(team).split(',') + str(brazil_team).split(','), user_name, component, project, jira_server
+        str(team).split(','), user_name, component, project, jira_server
     )
 
-    jiraProxy.update_data()
-    jiraProxy.make_velocities()
-    jiraProxy.make_velocities(False)
+    parser = ArgumentParser()
+    parser.add_argument("-u", "--update",
+                        help="update data from previous week",
+                        default=False)
+    parser.add_argument("-v", "--velocities",
+                        default=None,
+                        help="make velocities graphics for team or for every person")
+
+    args = parser.parse_args()
+    if args.update:
+        jiraProxy.update_data()
+    elif args.velocities is not None:
+        jiraProxy.make_velocities(args.velocities)
