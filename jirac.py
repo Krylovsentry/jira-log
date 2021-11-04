20f7d33fc70ffc40d9691ef60dba8c92843c3f98
Krylovsentry <anton.krylov@netcracker.com>
2021-11-04 13:55:09 +0300

    rewrite to jira python api

--- jirac.py
+++ jirac.py
@@ -1,7 +1,7 @@
 import configparser
 import os
+from datetime import datetime, timedelta
 
-from argparse import ArgumentParser
 from jira import JIRA
 
 from jirap import JiraProxy
@@ -17,8 +17,8 @@ def create_config(path):
     config.set("Settings", "project", "project name")
     config.set("Settings", "user_name", "your username")
     config.set("Settings", "password", "your password")
-    config.set("Settings", "team", "team assignie codes")
-    config.set("Settings", "component", "сomponent label")
+    config.set("Settings", "team", "team assignee codes")
+    config.set("Settings", "components", "сomponent labels")
 
     with open(path, "w") as config_file:
         config.write(config_file)
@@ -36,33 +36,30 @@ def crud_config(path):
         config.get("Settings", "user_name"),
         config.get("Settings", "password"),
         config.get("Settings", "team"),
-        config.get("Settings", "component")
+        config.get("Settings", "components")
     ]
 
 
 if __name__ == "__main__":
     path = "settings.ini"
-    [jira_server, project, user_name, password, team, component] = crud_config(path)
+    [jira_server, project, user_name, password, team, components] = crud_config(path)
 
     # initialize jira api
     jira_options = {'server': jira_server}
     jiraProxy = JiraProxy(
         JIRA(options=jira_options, basic_auth=(user_name, password)),
-        str(team).split(','), user_name, component, project, jira_server
+        str(team).split(','), user_name, str(components).split(','), project, jira_server
     )
 
-    # parser = ArgumentParser()
-    # parser.add_argument("-u", "--update",
-    #                     help="update data from previous week",
-    #                     default=False)
-    # parser.add_argument("-v", "--velocities",
-    #                     default=None,
-    #                     help="make velocities graphics for team or for every person")
-    #
-    # args = parser.parse_args()
-    # if args.update:
-    #     jiraProxy.update_data()
-    # elif args.velocities is not None:
-    #     jiraProxy.make_velocities(args.velocities)
-
-
+    # issues = jiraProxy.search_issues("filter = 5889579 and summary ~ 'UX' and resolution = Unresolved ")
+    # print(issues)
+    # jiraProxy.bulk_issue_update(issues, {'label': 'UI/UX'})
+    # jiraProxy.check_un_estimate_tasks()
+    jiraProxy.update_data(weeks=4, labels="'DS', 'Build'")
+    jiraProxy.make_velocities()
+    jiraProxy.make_velocities(False)
+    # created_tickets = jiraProxy.get_medium_count_of_tickets_create(label='Release4.05', weeks=8, priority='Major')
+    # # time_for_bugs = jiraProxy.get_medium_time_for_bugs('Release4.05', 4)
+    # print(f"time for 1 bug: {time_for_bugs}")
+    # print(f"created ticket median: {created_tickets}")
+    # print(f"time for bugs: {created_tickets * time_for_bugs}")
